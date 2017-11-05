ff30f2a50cd9e468608c5244972095fb635fcbc6
Nicolas Schoonbroodt <nicolas@elsewhere.be>
2017-11-05 08:38:23 -0800

    Partial waypoint updater (really simple)

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -5,6 +5,7 @@ from geometry_msgs.msg import PoseStamped
 from styx_msgs.msg import Lane, Waypoint
 
 import math
+import tf
 
 '''
 This node will publish waypoints from the car's current position to some `x` distance ahead.
@@ -23,7 +24,6 @@ TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 
 LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
 
-
 class WaypointUpdater(object):
     def __init__(self):
         rospy.init_node('waypoint_updater')
@@ -36,17 +36,32 @@ class WaypointUpdater(object):
 
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
-        # TODO: Add other member variables you need below
+        # Internal members
+        self.waypoints = None
+        self.nb_wp = 0
+        self.pose = None
+        self.last_idx = -1
 
         rospy.spin()
 
     def pose_cb(self, msg):
-        # TODO: Implement
-        pass
+        self.pose = msg.pose
+        
+        if self.waypoints is None:
+            return
+        
+        if self.last_idx < 0:
+            # initial case
+            self.initial_index_update()
+        
+        self.index_update()
 
     def waypoints_cb(self, waypoints):
-        # TODO: Implement
-        pass
+        if self.waypoints is None:
+            self.waypoints = waypoints.waypoints
+            self.nb_wp = len(self.waypoints)
+        else:
+            rospy.logerr('We have got more than one base waypoint definition')
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
@@ -69,6 +84,54 @@ class WaypointUpdater(object):
             dist += dl(waypoints[wp1].pose.pose.position, waypoints[i].pose.pose.position)
             wp1 = i
         return dist
+    
+    def initial_index_update(self):
+        rospy.logdebug("Initial index update")
+        # find the closest WP (will manage the car direction afterwards, using the index_update function, called in the callback)
+        min_dist = float("inf")
+        min_idx = -1
+        
+        dl = lambda a, b: math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2  + (a.z-b.z)**2)
+        
+        #rospy.logerr("Initial called %s", self.pose)
+        for idx, wp in enumerate(self.waypoints):
+            d = dl(self.pose.position, wp.pose.pose.position)
+            if d <= min_dist:
+                min_dist = d
+                self.last_idx = idx
+        
+        if self.last_idx == -1:
+            rospy.logerr("Something has gone horribly wrong here, probably an empty WP initialisation?")
+        else:
+            rospy.loginfo("First waypoint index at initialisation: %s", self.last_idx)
+            
+    def index_update(self):
+        # check if the wp is in front of car. If not, takes the next one
+        
+        # we're a car, 2D geometry should be good enough
+        _,_,yaw = tf.transformations.euler_from_quaternion(
+            [self.pose.orientation.x,
+             self.pose.orientation.y,
+             self.pose.orientation.z,
+             self.pose.orientation.w])
+        
+        while self.last_idx < self.nb_wp:
+            dx = self.waypoints[self.last_idx].pose.pose.position.x-self.pose.position.x
+            dy = self.waypoints[self.last_idx].pose.pose.position.y-self.pose.position.y
+            
+            dx_in_car = math.cos(yaw)*dx + math.sin(yaw)*dy
+            if dx > 0:
+                break
+            self.last_idx += 1
+        rospy.logdebug("Current WP index: %s", self.last_idx)
+        
+        lane = Lane()
+        lane.waypoints = self.waypoints[self.last_idx:self.last_idx+LOOKAHEAD_WPS]
+        lane.header.frame_id = '/world'
+        lane.header.stamp = rospy.Time.now()
+        
+        self.final_waypoints_pub.publish(lane)
+        
 
 
 if __name__ == '__main__':
